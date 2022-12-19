751a9763d58e81838c27758c6ac86b1aaa624367
Sadia <snaushin944@gmail.com>
2022-12-19 16:23:42 +0900

    changed files

--- main.js
+++ main.js
@@ -1,49 +1,27 @@
 
-// let acc = document.getElementsByClassName('accordion');
 
-// Array.from(acc).forEach(e => { e.addEventListener('click', activeAcc)})
 
-// function activeAcc() {
-//   this.classList.toggle('active')
-//   let panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     }
-//     let arrow = this.getElementsByTagName('img')[0]
-//     arrow.classList.toggle('up')
-// }
-
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("faq");
 var i;
 
-// Open the first accordion
-var firstAccordion = acc[0];
-var firstPanel = firstAccordion.nextElementSibling;
+var firstFaq = acc[0];
+var firstPanel = firstFaq.nextElementSibling;
 
-// Add onclick listener to every accordion element
 for (i = 0; i < acc.length; i++) {
   acc[i].onclick = function() {
-    // For toggling purposes detect if the clicked section is already "active"
     var isActive = this.classList.contains("active");
-    // Rotate arrow icon
     let arrow = this.getElementsByClassName('arrow')[0]
     let upArrow = arrow.classList.contains('up')
-    // arrow.classList.add('up')
 
-    // Close all accordions
-    var allAccordions = document.getElementsByClassName("accordion");
+    var allFaqs = document.getElementsByClassName("faq");
     let allArrows = document.getElementsByClassName('arrow')
-    for (j = 0; j < allAccordions.length; j++) {
-      // Remove active class from section header
-      allAccordions[j].classList.remove("active");
+    for (j = 0; j < allFaqs.length; j++) {
+      allFaqs[j].classList.remove("active");
       allArrows[j].classList.remove('up');
       
       
 
-      // Remove the max-height class from the panel to close it
-      var panel = allAccordions[j].nextElementSibling;
+      var panel = allFaqs[j].nextElementSibling;
       var maxHeightValue = getStyle(panel, "maxHeight");
     
     if (maxHeightValue !== "0px") {
@@ -76,22 +54,16 @@ for (i = 0; i < acc.length; i++) {
   };
 }
 
-  // Cross-browser way to get the computed height of a certain element. Credit to @CMS on StackOverflow (http://stackoverflow.com/a/2531934/7926565)
   function getStyle(el, styleProp) {
     var value, defaultView = (el.ownerDocument || document).defaultView;
-    // W3C standard way:
     if (defaultView && defaultView.getComputedStyle) {
-      // sanitize property name to css notation
-      // (hypen separated words eg. font-Size)
       styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
       return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
-    } else if (el.currentStyle) { // IE
-      // sanitize property name to camelCase
+    } else if (el.currentStyle) { 
       styleProp = styleProp.replace(/\-(\w)/g, function(str, letter) {
         return letter.toUpperCase();
       });
       value = el.currentStyle[styleProp];
-      // convert other units to pixels on IE
       if (/^\d+(em|pt|%|ex)?$/i.test(value)) { 
         return (function(value) {
           var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
